2712589c32ef72814241229ef2214c5f493596d3
XcelevatorKeerthika <keerthika.2514@xcelevate.org>
2025-07-22 16:21:19 +0530

    Update Fcp004.java

--- Fcp004.java
+++ Fcp004.java
@@ -1,18 +1,18 @@
 public class Fcp004{
 public static void main(String[]args){
 int a=15;
-int b=5;
-int c=a+b;
-int d=a-b;
-int e=a*b;
-double f=a/b;
-int g=a%b;
-int h=a/b;
-System.out.println(c);
-System.out.println(d);
-System.out.println(e);
-System.out.println(f);
-System.out.println(g);
-System.out.println(h);
+int b=4;
+int add=a+b;
+int sub=a-b;
+int multiple=a*b;
+double divide=(double)a/b;
+int modulo=a%b;
+int intfloor=a/b;
+System.out.println(add);
+System.out.println(sub);
+System.out.println(multiple);
+System.out.println(divide);
+System.out.println(modulo);
+System.out.println(intfloor);
 }
 }
