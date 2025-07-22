e341e97709ff8d6919e7c6af9a9d19c18c8d7d1f
XcelevatorKeerthika <keerthika.2514@xcelevate.org>
2025-07-22 16:35:44 +0530

    Update Fcp006.java

--- Fcp006.java
+++ Fcp006.java
@@ -1,10 +1,12 @@
 public class Fcp006{
 public static void main(String[]args){
 int a=843;
-int b=321;
+if(a>0){
 int c=a%10;
-int d=b%10;
 System.out.println(c);
-System.out.println(d);
+}
+else{
+System.out.println("Invalid input number");
+}
 }
 }
