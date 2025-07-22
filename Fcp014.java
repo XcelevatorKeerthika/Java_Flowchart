09efd9689cc03e0811884ebe9ba05f31a57e0d1c
XcelevatorKeerthika <keerthika.2514@xcelevate.org>
2025-07-22 22:41:19 +0530

    Fcp014

---
+++ Fcp014.java
@@ -0,0 +1,23 @@
+public class Fcp014{
+public static void main(String[]args){
+int a=80;
+int b=50;
+int c=43;
+if(a>b){
+if(a>c){
+System.out.println(a);
+}
+else{
+System.out.println(c);
+}
+}
+else{
+if(b>c){
+System.out.println(b);
+}
+else{
+System.out.println(c);
+}
+}
+}
+}
