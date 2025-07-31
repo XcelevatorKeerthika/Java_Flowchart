4022b3a4fc323e268b52787c56f8dfd3f5811a30
XcelevatorKeerthika <keerthika.2514@xcelevate.org>
2025-07-31 22:15:24 +0530

    Update Fcp002.java

--- Fcp002.java
+++ Fcp002.java
@@ -1,5 +1,7 @@
+import java.util.*;
 public class Fcp002{
 public static void main(String[]args){
-int number=20;
+Scanner sc=new Scanner(System.in);
+int number=sc.nextInt();
 System.out.println(number);
 }}
