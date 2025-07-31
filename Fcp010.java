505e04b9db2404032ad145dcc02f8ef038773705
XcelevatorKeerthika <keerthika.2514@xcelevate.org>
2025-07-31 23:21:45 +0530

    Fcp010.java

--- Fcp010.java
+++ Fcp010.java
@@ -1,7 +1,9 @@
+import java.util.*;
 public class Fcp010{
 public static void main(String[]args){
-int a=20;
-int b=3;
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
 if(a>b){
 System.out.println(a);
 }
