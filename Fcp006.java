03b30233754393eb930a0bd6b584d3fcb6c35b32
XcelevatorKeerthika <keerthika.2514@xcelevate.org>
2025-07-31 23:01:41 +0530

    Update Fcp006.java

--- Fcp006.java
+++ Fcp006.java
@@ -1,6 +1,8 @@
+import java.util.*;
 public class Fcp006{
 public static void main(String[]args){
-int a=843;
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
 if(a>0){
 int c=a%10;
 System.out.println(c);
