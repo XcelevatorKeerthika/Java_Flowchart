6dda1cbdcfd2fbb418d74ae58cd18cf758544c27
XcelevatorKeerthika <keerthika.2514@xcelevate.org>
2025-07-31 23:01:09 +0530

    Fcp007.java

--- Fcp007.java
+++ Fcp007.java
@@ -1,6 +1,8 @@
+import java.util.*;
 public class Fcp007{
 public static void main(String[]args){
-int celsius=30;
+Scanner sc=new Scanner(System.in);
+int celsius=sc.nextInt();
 int fahrenheit=(celsius*9)/5+32;
 System.out.println(fahrenheit);
 }
