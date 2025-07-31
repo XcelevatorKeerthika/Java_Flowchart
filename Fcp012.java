cdae654db2b04cf3ad9b122b0b507cc1cc9a3dd2
XcelevatorKeerthika <keerthika.2514@xcelevate.org>
2025-07-31 23:41:39 +0530

    Update Fcp012.java

--- Fcp012.java
+++ Fcp012.java
@@ -1,14 +1,18 @@
+import java.util.*;
 public class Fcp012{
 public static void main(String[]args){
-int a=6;
-if(a==0){
-System.out.println("Zero");
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+if(a<0){
+System.out.println("Negative");
 }
 if(a>0){
 System.out.println("Positive");
 }
 else{
-System.out.println("Negative");
+if(a==0){
+System.out.println("Zero");
+}
 }
 }
 }
