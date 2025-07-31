a7c394a47ddc842eed9e9e57284c17e696860c12
XcelevatorKeerthika <keerthika.2514@xcelevate.org>
2025-07-31 22:13:27 +0530

    Update Fcp001.java

--- Fcp001.java
+++ Fcp001.java
@@ -1,8 +1,10 @@
+import java.util.*;
 public class Fcp001{
 public static void main(String[]args){
-int birthdate=22;
-int birthyear=2001;
-String birthmonth="May";
+Scanner sc=new Scanner(System.in);
+String birthmonth=sc.nextLine();
+int birthdate=sc.nextInt();
+int birthyear=sc.nextInt();
 System.out.println(birthdate);
 System.out.println(birthyear);
 System.out.println(birthmonth);
