f7548b1b5c6e0e41c95cf8071d26f98e2afe589a
XcelevatorKeerthika <keerthika.2514@xcelevate.org>
2025-07-31 23:15:35 +0530

    Fcp009.java

--- Fcp009.java
+++ Fcp009.java
@@ -1,7 +1,9 @@
+import java.util.*;
 public class Fcp009{
 public static void main(String[]args){
-int a=15;
-int b=4;
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
 a=a-1;
 int c=a%b;
 int d=a-c;
