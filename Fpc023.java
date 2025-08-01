154426cb790c45a6b58db2a3b7e7edde0188bd68
XcelevatorKeerthika <keerthika.2514@xcelevate.org>
2025-08-01 10:06:55 +0530

    FCP023

---
+++ Fpc023.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Fpc023{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+sc.nextLine();
+String name=sc.nextLine();
+int i=1;
+for(i=1;i<=n;i++){
+System.out.println(name);
+}
+}}
